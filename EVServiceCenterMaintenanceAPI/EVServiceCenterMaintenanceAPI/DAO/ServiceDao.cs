5d8a47b5930e933bc29e8075fa1371b408d1e98f
NguyenNgocTheAn <thinghi73@gmail.com>
2025-10-28 22:04:22 +0700

    add function check exist service center

--- EVServiceCenterMaintenanceAPI/EVServiceCenterMaintenanceAPI/DAO/ServiceDao.cs
+++ EVServiceCenterMaintenanceAPI/EVServiceCenterMaintenanceAPI/DAO/ServiceDao.cs
@@ -14,6 +14,11 @@ namespace EVServiceCenterMaintenanceAPI.DAO
             _context = context;
         }
 
+        public Task<bool> IsExistServiceCenterAsync(int serviceCenterId)
+        {
+            return _context.ServiceCenters.AnyAsync(s => s.CenterId == serviceCenterId);
+        }
+
         public async Task<(List<Service> Services, int Total)> GetAllServicesAsync(ServiceQueryParams queryParams)
         {
             var validation = queryParams.Validate();
