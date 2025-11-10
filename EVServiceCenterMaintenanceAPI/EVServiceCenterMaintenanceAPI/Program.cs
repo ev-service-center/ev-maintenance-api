d2a33ffa6f47a0f5e8cce104ca177ac39892af33
nvkhang00 <nvkhang0099@gmail.com>
2025-11-11 01:11:25 +0700

    add endpoint PartUsage API

--- EVServiceCenterMaintenanceAPI/EVServiceCenterMaintenanceAPI/Program.cs
+++ EVServiceCenterMaintenanceAPI/EVServiceCenterMaintenanceAPI/Program.cs
@@ -102,6 +102,7 @@ builder.Services.AddScoped<ServiceCenterDao>();
 builder.Services.AddScoped<EmployeeDao>();
 builder.Services.AddScoped<ReminderDao>();
 builder.Services.AddScoped<MaintenanceHistoryDao>();
+builder.Services.AddScoped<PartUsageDao>();
 builder.Services.AddScoped<AppointmentSlotGeneratorService>();
 builder.Services.AddHostedService<SlotGenerationBackgroundService>();
 builder.Services.AddScoped<AppointmentSlotDao>();
